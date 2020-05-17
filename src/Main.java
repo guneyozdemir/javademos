b96aed8bb81f17459b2064e13e6b4ad60f65a850
Güney Özdemir <guneyozzdemir@gmail.com>
2020-05-17 11:31:34 +0300

    first application

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int sayi=20;
+        if(sayi>20){
+            System.out.println("Sayı 20'ye eşittir.");
+        }
+        else if (sayi>19){
+            System.out.println("Sayı 19'dan büyüktür.");
+        }
+    }
+
+}
