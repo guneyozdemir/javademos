13f9a0971f60206665a952e690da265da7adf756
Güney Özdemir <guneyozzdemir@gmail.com>
2020-05-17 12:05:17 +0300

    if else variable sample fixed

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	// write your code here
         int sayi=20;
         if(sayi>20){
-            System.out.println("Sayı 20'ye eşittir.");
+            System.out.println("Sayı 20'den Büyüktür.");
         }else if (sayi>19){
             System.out.println("Sayı 19'dan büyüktür.");
         }else{
